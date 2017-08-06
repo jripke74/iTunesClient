038b4e3c165d8024e0b8103abdd060d342523206
Jeff Ripke <jripke74@gmail.com>
2017-08-06 16:17:42 -0700

    updated artist model

--- iTunesClient/Artist.swift
+++ iTunesClient/Artist.swift
@@ -13,12 +13,14 @@ class Artist {
     let name: String
     let primaryGenre: Genre
     var albums: [Album]
+    let artistWebsite: String
     
-    init(id: Int, name: String, primaryGenre: Genre, albums: [Album]) {
+    init(id: Int, name: String, primaryGenre: Genre, albums: [Album], artistWebsite: String) {
         self.id = id
         self.name = name
         self.primaryGenre = primaryGenre
         self.albums = albums
+        self.artistWebsite = artistWebsite
     }
 }
 
@@ -28,13 +30,16 @@ extension Artist {
             static let artistName = "artistName"
             static let artistId = "artistId"
             static let primaryGenreId = "primaryGenreId"
+            static let artistLinkUrl = "artistLinkUrl"
         }
         guard let artistName = json[Key.artistName] as? String,
                let artistId = json[Key.artistId] as? Int,
                let primaryGenreId = json[Key.primaryGenreId] as? Int,
-        let primaryGenreValue = Genre(rawValue: primaryGenreId) else {
-            return nil
-        }
-        self.init(id: artistId, name: artistName, primaryGenre: primaryGenreValue, albums: [])
+               let primaryGenreValue = Genre(rawValue: primaryGenreId),
+               let artistLinkUrl = json[Key.artistLinkUrl] as? String
+               else {
+                  return nil
+               }
+        self.init(id: artistId, name: artistName, primaryGenre: primaryGenreValue, albums: [], artistWebsite: artistLinkUrl)
     }
 }
